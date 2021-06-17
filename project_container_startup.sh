e5af5fd40e7d26d782bbca85fbf36a80db13e1a6
Rajagopalan Gangadharan <rajagopalan.gangadharan@appdynamics.com>
2021-06-17 19:13:03 +0530

    Entry point visibility

--- project_container_startup.sh
+++ project_container_startup.sh
@@ -5,6 +5,11 @@ curl https://raw.githubusercontent.com/RAJAGOPALAN-GANGADHARAN/AdCapital-Script-
 curl https://raw.githubusercontent.com/RAJAGOPALAN-GANGADHARAN/AdCapital-Script-Box/main/appdynamics_container_config.sh -o appdynamics_container_config.sh
 curl https://raw.githubusercontent.com/RAJAGOPALAN-GANGADHARAN/AdCapital-Script-Box/main/appdynamics_container_startup.sh -o appdynamics_container_startup.sh
 
+cp adcapital_load_startup.sh /scripts/adcapital_load_startup.sh
+cp adcapital_tomcat_startup.sh /scripts/adcapital_tomcat_startup.sh
+cp appdynamics_container_config.sh /scripts/appdynamics_container_config.sh
+cp appdynamics_container_startup.sh /scripts/appdynamics_container_startup.sh
+
 apt-get install -y git
 
 curl -O https://downloads.gradle.org/distributions/gradle-2.1-bin.zip
