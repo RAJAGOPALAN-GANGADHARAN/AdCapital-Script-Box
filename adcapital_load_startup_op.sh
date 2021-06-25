94fea66e9b61490aa3707dfaba9c10caf4c8e53d
Rajagopalan Gangadharan <rajagopalan.gangadharan@appdynamics.com>
2021-06-25 12:57:11 +0530

    Keep OP for backwards compatibility

---
+++ adcapital_load_startup_op.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+apt-get update
+apt-get install -y git
+
+#DOCKERIZE
+DOCKERIZE_VERSION=v0.5.0
+wget https://github.com/jwilder/dockerize/releases/download/$DOCKERIZE_VERSION/dockerize-linux-amd64-$DOCKERIZE_VERSION.tar.gz
+tar -C /usr/local/bin -xzvf dockerize-linux-amd64-$DOCKERIZE_VERSION.tar.gz
+rm dockerize-linux-amd64-$DOCKERIZE_VERSION.tar.gz
+
+# Gradle
+curl -O -L http://downloads.gradle.org/distributions/gradle-2.1-bin.zip
+unzip gradle-2.1-bin.zip -d /opt/
+rm gradle-2.1-bin.zip
+GRADLE_HOME=/opt/gradle-2.1
+PATH=$PATH:$GRADLE_HOME/bin
+
+# Clone source repo from GitHub
+git clone https://github.com/Appdynamics/AD-Capital-Load.git
+
+LOAD_GEN_PROJECT=/AD-Capital-Load
+cd ${LOAD_GEN_PROJECT}; gradle distZip
+unzip ${LOAD_GEN_PROJECT}/build/distributions/load-generator.zip -d /
+chmod +x ${LOAD_GEN_PROJECT}/bin/*
+
+# 1st param to LoadRunner call
+if [ -z "${PORTAL_HOST}" ]; then
+        export PORTAL_HOST="portal";
+fi
+
+# 2nd param to LoadRunner call
+if [ -z "${PROCESSOR_HOST}" ]; then
+        export PROCESSOR_HOST="processor";
+fi
+
+# Service dependencies
+dockerize -wait tcp://${PORTAL_HOST}:8080 \
+          -wait tcp://${PROCESSOR_HOST}:8080 \
+          -wait-retry-interval ${RETRY} -timeout ${TIMEOUT} || exit $?
+
+${LOAD_GEN_PROJECT}/bin/AD-Capital-Load ${PORTAL_HOST} ${PROCESSOR_HOST}
---
+++ project_container_startup_op.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+curl https://raw.githubusercontent.com/RAJAGOPALAN-GANGADHARAN/AdCapital-Script-Box/main/adcapital_load_startup_op.sh -o adcapital_load_startup.sh
+curl https://raw.githubusercontent.com/RAJAGOPALAN-GANGADHARAN/AdCapital-Script-Box/main/adcapital_tomcat_startup_op.sh -o adcapital_tomcat_startup.sh
+curl https://raw.githubusercontent.com/RAJAGOPALAN-GANGADHARAN/AdCapital-Script-Box/main/appdynamics_container_config_op.sh -o appdynamics_container_config.sh
+curl https://raw.githubusercontent.com/RAJAGOPALAN-GANGADHARAN/AdCapital-Script-Box/main/appdynamics_container_startup_op.sh -o appdynamics_container_startup.sh
+
+cp adcapital_load_startup.sh /appdynamics/adcapital_load_startup.sh
+cp adcapital_tomcat_startup.sh /project/adcapital_tomcat_startup.sh
+cp appdynamics_container_config.sh /appdynamics/appdynamics.sh
+cp appdynamics_container_startup.sh /appdynamics/appdynamics_container_startup.sh
+
+
+apt-get install -y git
+
+curl -O https://downloads.gradle.org/distributions/gradle-2.1-bin.zip
+unzip gradle-2.1-bin.zip -d /opt
+rm gradle-2.1-bin.zip
+
+GRADLE_HOME=/opt/gradle-2.1
+PATH=$PATH:$GRADLE_HOME/bin
+
+# Clone source repo from GitHub
+git clone -b logging --single-branch https://github.com/Appdynamics/AD-Capital.git
+
+# Gradle build
+cd /AD-Capital; gradle build uberjar
+
+cp -r /AD-Capital ${PROJECT}
+echo "AD-Capital project copied to docker volume"
+
+cp -r /opt/gradle-2.1 ${PROJECT}
+echo "GRADLE project copied to docker volume"
